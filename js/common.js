4d2fa1847509e03e04159ac1b0eed4c8059e081d
Serge Goncharov <sgonch@hotmail.com>
2023-11-07 17:07:25 -0500

    New Design

---
+++ js/common.js
@@ -0,0 +1,74 @@
+/* https://www.w3web.net/how-to-add-header-and-footer-in-javascript/ */
+
+function header () {
+  with(document) {
+       write("<div id='headerMain'>");
+            write("<a class='logo' href='./index.html'>");
+              write("<span class='nav-title'>Julie Goncharov</span>");
+            write("</a>");
+           write("<div class='menuMain'>");
+               write("<ul class='menu'>");
+                    write("<li><a href='index.html'>Home</a>");
+                    write("</li>")
+                    write("<li><a href='publications.html'>Publications</a>");
+                    write("</li>")
+                    write("<li><a href='projects.html'>Projects</a>");
+                    write("</li>")
+                    write("<li><a href='contact_me.html'>Contact me</a>");
+                    write("</li>")
+ //                   write("<li><a href='mailto:julie.goncharov@uni-goettingen.de'>Contact us</a>");
+ //                   write("</li>")
+               write("</ul>");
+           write("</div>");
+        write("</div>");
+/*      write("<div id='project-header'>");
+            write("<image class='project-header-image' src='./assets/images/AIAI_prog.png'/>");
+                write("<div class='main-title'>Agency and Intentions in AI</div>");
+                  write("<div>");
+                      write("<blockquote><p>How do you connect with a single piece of information?</p><p>Date ‘em</p>");
+                          write("<footer><cite>Kyle Thompson</cite></footer>");
+                      write("</blockquote>");
+                write("</div>");
+            write("</div>");
+      write("</div>"); */
+  }
+  /* imgsearch(); */
+}
+
+function footerContainer(){
+      with(document){
+        write("<div id='footerMain'>");
+            write("<div>");
+                    write("<a target='_blank' href='https://ail-workshop.github.io/'>");
+                        write("<img src='./assets/images/ail_logo.png' style='width:120px'>");
+                    write("</a>");
+            write("</div>");
+            write("<div class='project-card-text-container'>");
+                    write("<a class='button' target='_blank' href='https://ail-workshop.github.io/'>");
+                        write("Agency and Intentions in Language");
+                    write("</a>");
+            write("</div>");
+          write("<div>");
+                  write("<a target='_blank' href='https://uni-goettingen.de/de/vorschau_3dd0fb16351b3f7ffbd9a226d14b3a9b/652812.html'>");
+                      write("<img src='./assets/images/ASG_logo.png' style='width:120px'>");
+                  write("</a>");
+          write("</div>");
+          write("<div class='project-card-text-container'>");
+                  write("<a class='button' target='_blank' href='https://uni-goettingen.de/de/vorschau_3dd0fb16351b3f7ffbd9a226d14b3a9b/652812.html'>");
+                      write("Action Sensitivity in Grammar");
+                  write("</a>");
+          write("</div>");
+          write("<div>");
+                  write("<a target='_blank' href='https://ail-workshop.github.io/'>");
+                      write("<img src='./assets/images/AIAi_prog.png' style='width:120px'>");
+                  write("</a>");
+          write("</div>");
+          write("<div class='project-card-text-container'>");
+                  write("<a class='button' target='_blank' href='https://ail-workshop.github.io/aiai-conference/'>");
+                      write("Agency and Intentions in AI");
+                  write("</a>");
+          write("</div>");
+ 
+        write("</div>");
+          }
+  }
